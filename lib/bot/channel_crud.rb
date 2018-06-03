c90a11ca5eea0045325d85848a5d7d85737d84b9
Micke Lisinge <me@mike.gg>
2018-06-03 18:01:31 +0200

    Check in missing ChannelCrud file

---
+++ lib/bot/channel_crud.rb
@@ -0,0 +1,19 @@
+module Bot
+  module ChannelCrud
+    extend Discordrb::EventContainer
+
+    TEXT_CHANNEL_TYPE = 0
+
+    channel_create type: TEXT_CHANNEL_TYPE do |event|
+      Channel.upsert!(event.channel)
+    end
+
+    channel_update type: TEXT_CHANNEL_TYPE do |event|
+      Channel.upsert!(event.channel)
+    end
+
+    channel_delete type: TEXT_CHANNEL_TYPE do |event|
+      Channel.destroy(event.id)
+    end
+  end
+end
