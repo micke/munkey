ccdb42f0ce979a341f19d36b89428b03bd685040
Micke Lisinge <me@mike.gg>
2018-06-05 13:31:42 +0200

    Fix cop

--- db/migrate/7_create_channels.rb
+++ db/migrate/7_create_channels.rb
@@ -5,13 +5,9 @@ class CreateChannels < ActiveRecord::Migration[5.1]
       t.bigint :server_id
       t.string :topic
       t.boolean :gb_alerts, default: false
-      t.timestamps null: false, default: -> { 'NOW()' }
+      t.timestamps null: false, default: -> { "NOW()" }
     end
 
     execute "ALTER TABLE channels ADD PRIMARY KEY (id);"
-  end
-
-  def down
-
   end
 end
